f544207aa63eb93d312f53d8fe0a385d9916153d
xElroy <sobokonst@gmail.com>
2019-10-25 22:51:33 +0300

    modify bvi and add experiments file

--- source/bvi.py
+++ source/bvi.py
@@ -103,6 +103,9 @@ class BVI(object):
         
         # return the initialized result
         return best_param
+    
+    def get_weights(self):
+        return self.weights.data
 
     def _compute_weights(self):
         raise NotImplementedError
