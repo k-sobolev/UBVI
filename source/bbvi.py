0435a7e6376106212b87124ebbbe0a4804b8d9da
NurislamT <30591533+NurislamT@users.noreply.github.com>
2019-10-25 21:04:36 +0300

    Add files via upload

--- source/bbvi.py
+++ source/bbvi.py
@@ -2,14 +2,23 @@ import numpy as np
 import torch
 from bvi import BVI
 
-
-def logsumexp(input):
-	return torch.log(torch.sum(torch.exp(input)))
+def projection_on_simplex(x):
+    u = torch.sort(x, descending=True)[0]
+    indices = torch.arange(1, u.shape[0] + 1)
+    rho_nz = u + 1. / indices * (1. - torch.cumsum(u, dim=0)) > 0
+    rho = indices[rho_nz].max()
+    lmbda = 1. / rho * (1. - u[:rho].sum())
+    out = torch.max(
+        torch.stack([x + lmbda, torch.zeros_like(x, dtype=torch.float32)]), dim=0).values
+    return out / out.sum()
 
 class BBVI(BVI):
     
-    def __init__(self, logp, component_dist, opt_alg, lmb = lambda itr : 1, n_samples = 100, n_simplex_iters = 3000, eps = None, **kw):
-        super().__init__(component_dist, opt_alg, **kw)
+    def __init__(
+        self, logp, component_dist, lmb = lambda itr : 1,
+        n_samples = 100, n_simplex_iters = 3000, eps = None, **kw
+    ):
+        super().__init__(component_dist, **kw)
         self.logp = logp
         self.lmb = lmb
         self.n_samples = n_samples
@@ -18,47 +27,64 @@ class BBVI(BVI):
     
     def _compute_weights(self):
         if self.params.shape[0] == 1:
-            return 1
+            return torch.tensor([1.])
         else:
-            obj = lambda z, i: self._kl_estimate(self.params, z)
-            grd = grad(obj)
-            x = torch.ones(self.params.shape[0])/float(self.params.shape[0])
-            return simplex_sgd(x, obj, grd, learning_rate=lambda itr : 0.1/(1+itr), num_iters=self.n_simplex_iters, callback = self._print_perf_w if self.verbose else None)
+            weights = torch.ones(self.params.shape[0], dtype=torch.float32)
+            weights /= weights.sum()
+
+            for j in range(self.n_simplex_iters):
+                weights.requires_grad_()
+                optimizer = torch.optim.SGD([weights], lr=self.lr)
+
+                optimizer.zero_grad()
+                self._kl_estimate(self.params, weights).backward()
+                optimizer.step()
+
+                weights = projection_on_simplex(weights.detach())
+
+        return weights
 
     def _error(self):
         return "KL Divergence", self._kl_estimate(self.params, self.weights)
     
     def _objective(self, x, itr):
-        h_samples = self.component_dist.sample(x, self.n_samples)
-        #compute log target density under samples
+        h_samples = self.component_dist.generate_samples_for_one_component(x, self.n_samples)
+        
+        # compute log target density under samples
         lf = self.logp(h_samples).mean()
-        #compute current log mixture density
-        if self.weights.size > 0:
-            lg = self.component_dist.logpdf(self.params, h_samples)
+        
+        # compute current log mixture density
+        if self.weights.shape[0] > 0:
+            lg = self.component_dist.log_pdf(self.params, h_samples)
             if len(lg.shape) == 1:
-                #need to add a dimension so that each sample corresponds to a row in lg
-                lg = lg[:,np.newaxis] 
-            #lg = logsumexp(lg+np.log(np.maximum(self.weights[-1], 1e-64)), axis=1).mean()
-            lg = lg[:, self.weights > 0] + torch.log(self.weights[self.weights>0])
+                # need to add a dimension so that each sample corresponds to a row in lg
+                lg = lg[:, None] 
+            
+            lg = lg[:, self.weights > 0] + torch.log(self.weights[self.weights > 0])
             if self.eps:
-                lg = torch.cat((lg, np.log(self.eps)*np.ones((lg.shape[0],1))), 1)
+                lg = torch.cat((lg, np.log(self.eps) * torch.ones_like((lg.shape[0], 1))), 1)
             lg = torch.logsumexp(lg, dim=1).mean()
         else:
             lg = 0.
-        lh = self.component_dist.logpdf(x, h_samples).mean()
-        return lg + self.lmb(self.weights.shape[0])*lh - lf
+        lh = self.component_dist.log_pdf(x, h_samples).mean()
+
+        # print(lg)
+        # print(lh)
+        # print(lf)
+
+        return lg + self.lmb(self.weights.shape[0]) * lh - lf
     
-    def _kl_estimate(self, prms, wts):
+    def _kl_estimate(self, params, weights):
         out = 0.
-        for k in range(wts.shape[0]):
-            samples = self.component_dist.sample(prms[k, :], self.n_samples)
-            lg = self.component_dist.logpdf(prms, samples)
-            if len(lg.shape)==1:
+        for k in range(weights.shape[0]):
+            samples = self.component_dist.generate_samples_for_one_component(params[k, :], self.n_samples)
+            lg = self.component_dist.log_pdf(params, samples)
+            if len(lg.shape) == 1:
                 lg = lg[:,np.newaxis]
-            #lg = logsumexp(lg+np.log(np.maximum(wts, 1e-64)), axis=1)
-            lg = torch.logsumexp(lg[:,wts>0] + torch.log(wts[wts>0]), dim=1)
+            
+            lg = torch.logsumexp(lg[:, weights > 0] + torch.log(weights[weights > 0]), dim=1)
             lf = self.logp(samples)
-            out += wts[k]*(lg.mean()-lf.mean())
+            out += weights[k] * (lg.mean() - lf.mean())
         return out
     
     def _print_perf_w(self, itr, x, obj, grd):
